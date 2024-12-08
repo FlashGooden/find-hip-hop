5ca41feae9caedaa254e35b348d2af99e0b3a0e3
FlashGooden <Tro12185@gmail.com>
2024-12-08 14:46:28 -0500

    feat: higher order layout + page

---
+++ src/app/page.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import MapComponent from './components/MapComponent';
+import FilterBar from './components/FilterBar';
+
+const HomePage: React.FC = () => {
+  const [filters, setFilters] = useState({
+    genre: '',
+    popularity: '',
+    location: '',
+  });
+
+  const handleFilterChange = (filterType: string, value: string) => {
+    setFilters(prevFilters => ({
+      ...prevFilters,
+      [filterType]: value,
+    }));
+  };
+
+  return (
+    <div>
+      <h1>Find Hip Hop</h1>
+      <FilterBar onFilterChange={handleFilterChange} />
+      <MapComponent filters={filters} />
+    </div>
+  );
+};
+
+export default HomePage;
